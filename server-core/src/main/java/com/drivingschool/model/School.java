3f7c0e38337b8f6b86b9cefa97761820a6369092
mmelki <mmelki@kapit.fr>
2017-01-21 16:40:13 +0100

    Add school entity

---
+++ server-core/src/main/java/com/drivingschool/model/School.java
@@ -0,0 +1,63 @@
+package com.drivingschool.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "schools")
+public class School implements Serializable {
+
+    @Id
+    private String id;
+    private String address;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admin_username")
+    private User admin;
+
+    @OneToMany
+    @JoinTable
+    private List<Card> cards;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public User getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(User admin) {
+        this.admin = admin;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+
+}
